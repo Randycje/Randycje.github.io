7d742b64ddfab256b9772bcc4a936f8f0a695429
Randy Chong <randychong.je@gmail.com>
2026-02-07 09:12:03 +0800

    fix bugs

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Relative base for GitHub Pages
+  base: '/', // Absolute base for User Pages (<username>.github.io)
 })
